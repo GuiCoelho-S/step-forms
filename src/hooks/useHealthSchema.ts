712003281599fa418134aa748613a61b499c1093
GuiCoelho-S <gs.coelho@outlook.com>
2022-06-26 17:15:36 -0300

    feat: create new fucntion for forms validate

--- src/hooks/useHealthSchema.ts
+++ src/hooks/useHealthSchema.ts
@@ -1,33 +1,55 @@
-import { useFormik } from "formik"
+import { useFormik } from "formik";
 import * as yup from "yup";
+import { validateCPF } from "../utils/cpfValidate";
 
-
+export const professions = ['Programador', 'Desenhista', 'Físico', 'Cozinheiro'];
 const healthYupShema = yup.object().shape({
-  name: yup.string().required("Nome obrigatório"),
-  email: yup.string().email().required(),
-  cpf: yup.string().required("Cpf é um dado obrigatório").typeError("Cpf inválido, verifique e tente novamente"),
+  name: yup
+    .string()
+    .required("Nome é obrigatório")
+    .test('isCorrectName','Sobrenome é necessário', (name) => {
+      if(name){
+        name = name?.trim();
+        let nameSplitted = name?.split(" ");
+        
+        return nameSplitted!.length >= 2;  
+      };
+      return false
+    }),
+  email: yup
+    .string()
+    .email()
+    .required("O email é necessário"),
+  cpf: yup
+    .string()
+    .required("Cpf é um dado obrigatório")
+    .test("isValidCPf", "Digite um CPF válido", (cpf) => {
+      if (cpf && cpf.length > 0) return validateCPF(cpf);
+    }),
   adress: yup.string().required(),
-  age: yup.number()
-  .required("O campo é obrigatório.")
-  .positive("O campo deve ser positivo.")
-  .integer("O campo deve ser um número inteiro."),
-})
+  age: yup.number().required("O campo é obrigatório."),
+  professions: yup.string().oneOf(professions, 'suhdsudhu')
+});
+
 export const useHealthSchema = () => {
-
-
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      cpf: '',
-      adress: '',
-      age:'',
+      name: "",
+      email: "",
+      cpf: "",
+      adress: "",
+      age: "",
+      professions: professions[0]
     },
+    validateOnBlur:true,
     validationSchema: healthYupShema,
-    onSubmit: (value) => console.log(value),
-    validateOnChange: true,
-    
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
   });
 
-  return { formikHealth: formik }
-}
+
+  return { formikHealth: formik };
+};
+
